a32afe78622864caa8af467f7d5464104137a786
ssUser <dimapestov113@gmail.com>
2026-01-16 23:34:05 +0300

    grid new

--- DiplomObsAlarm/MainPage.xaml.cs
+++ DiplomObsAlarm/MainPage.xaml.cs
@@ -10,11 +10,12 @@
 
         private async void OnAdminClicked(object? sender, EventArgs e)
         {
-            //await DisplayAlertAsync("Кнопка","Админ","Ок");
+            await DisplayAlertAsync("Кнопка","Админ","Ок");
             await Navigation.PushModalAsync(new AdminEnterPage());
         }
         private async void OnUserClicked(object? sender, EventArgs e)
         {
+
             await DisplayAlertAsync("Кнопка", "Пользователь", "Ок");
         }
     }
