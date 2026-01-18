01a1200e99312f03db28bf1683b6dcafbbb7eea8
ssUser <dimapestov113@gmail.com>
2026-01-19 00:13:13 +0300

    КАРОЧЕ ТУТ НАХУЙ НОВАЯ РАЗМЕТКА ЧЕРЕЗ C# И ОБРАЩЕНИЕ К НЕМУ ИЗ НУЖНЫХ ОКОН

--- DiplomObsAlarm/AdminEnterPage.xaml.cs
+++ DiplomObsAlarm/AdminEnterPage.xaml.cs
@@ -5,5 +5,7 @@ public partial class AdminEnterPage : ContentPage
 	public AdminEnterPage()
     {
         InitializeComponent();
+        GeneralSetting.RowDefinitions = GridPresets.AutoRow;
+        GeneralSetting.ColumnDefinitions = GridPresets.AutoCol;
     }
 }
---
+++ DiplomObsAlarm/AutoRC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Controls;
+
+namespace DiplomObsAlarm;
+
+public static class GridPresets
+{
+    public static RowDefinitionCollection AutoRows(int count)
+    {
+        var cols = new RowDefinitionCollection();
+        for (int i = 0; i < count; i++)
+            cols.Add(new RowDefinition { Height = GridLength.Auto });
+        return cols;
+    } 
+    public static ColumnDefinitionCollection AutoCols(int count)
+    {
+        var cols = new ColumnDefinitionCollection();
+        for (int i = 0; i < count; i++)
+            cols.Add(new ColumnDefinition { Width = GridLength.Auto });
+        return cols;
+    }//чёаниче
+    public static RowDefinitionCollection AutoRow => AutoRows(19);
+    public static ColumnDefinitionCollection AutoCol => AutoCols(40);
+}
